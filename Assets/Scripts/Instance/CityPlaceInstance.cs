2fb8fa18173730ae7debea4bfe48e2d2805c2e2b
hatomaru <111031779+hatomaru@users.noreply.github.com>
2026-02-08 15:20:42 +0900

    街が生まれるアニメーションを実装

---
+++ Assets/Scripts/Instance/CityPlaceInstance.cs
@@ -0,0 +1,50 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class CityPlaceInstance : MonoBehaviour
+{
+    readonly float delation = 0.4f;
+    readonly Vector3 defaultScale = new Vector3(0.0521533787f, 1.50971627f, 0.0533473007f);
+    [SerializeField] GameObject testCity;
+    public GameObject currentcityPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(testCity != null)
+        {
+            GenCity(testCity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenCity(GameObject obj)
+    {
+        GameObject city = Instantiate(obj, new Vector3(0, 0, 0), Quaternion.identity);
+        city.transform.SetParent(gameObject.transform);
+        long cityId = 0;
+        foreach(Transform parent in city.transform.parent.gameObject.GetComponentsInChildren<Transform>())
+        {
+            if(parent != city.transform && !parent.gameObject.CompareTag("Plane"))
+            {
+                Vector3 scale = parent.transform.localScale;
+                parent.transform.localScale = Vector3.zero;
+                cityId++;
+                parent.transform.DOScale(scale, (delation * 0.8f) * Random.Range(0.8f,1.5f)).SetEase(Ease.InOutBack).SetDelay(delation * 0.6f + ((delation * 0.02f) * cityId));
+            }
+        }
+        Vector3 localPos = new Vector3(-0.00941732153f, 6.91865826f, -0.00412968826f);
+        localPos.y = -1.5f;
+        city.transform.localPosition = localPos;
+        city.transform.localScale = defaultScale * 0f;
+        city.transform.DOLocalMoveY(6.918658f, delation * 1.2f).SetEase(Ease.InOutBack);
+        city.transform.DOScale(defaultScale * 0.8f, delation * 0.2f).SetEase(Ease.InOutExpo);
+        city.transform.DOScale(defaultScale, delation * 0.8f).SetEase(Ease.InOutBack).SetDelay(delation * 0.2f);
+        currentcityPrefab = city;
+    }
+}
