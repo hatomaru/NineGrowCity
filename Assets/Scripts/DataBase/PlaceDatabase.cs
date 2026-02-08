3dbe53b6d4db5966653b755c0b32d609a1972652
hatomaru <111031779+hatomaru@users.noreply.github.com>
2026-02-08 15:33:14 +0900

    土地情報を格納する場所を追加

---
+++ Assets/Scripts/DataBase/PlaceDatabase.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlaceDatabase : MonoBehaviour
+{
+    [SerializeField] PlaceData[] placeDatas;
+
+    /// <summary>
+    /// 指定したキーに対応する土地データを取得する
+    /// </summary>
+    /// <param name="key">検索したいキー</param>
+    /// <returns>検索結果</returns>
+    public PlaceData GetPlaceData(PlaceKey key)
+    {
+        foreach (var data in placeDatas)
+        {
+            if (data.key == key)
+            {
+                return data;
+            }
+        }
+        Debug.LogError($"PlaceData not found for key: {key}");
+        return null;
+    }
+}
